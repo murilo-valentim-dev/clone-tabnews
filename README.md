36430c600ae70451fccfd5dcecbc98aabc576900
Murilo Valentim <166670216+murilo-valentim-dev@users.noreply.github.com>
2024-12-03 01:29:27 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto sobre o tabnews
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Não desista, tudo vai dar certo no final! bora</h1>
+  return <h1>Não desista, tudo vai dar certo no final! bora</h1>;
 }
 
 export default Home;
